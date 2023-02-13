1158a82d60aa8aee4c7ab7f28b8583655adc9b92
sailesh kafle <saileshkafle@saileshs-MacBook-Pro.local>
2023-02-13 11:04:09 -0500

    added devops project

---
+++ src/main/java/com/devops/sailesh/HelloController.java
@@ -0,0 +1,17 @@
+package com.devops.sailesh;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
+
+}
+
